cfae8e369760d15710e4af345b8f50c5227c5fb4
hiphoplsy <hiphoplsy@naver.com>
2020-08-18 09:36:46 +0900

    ADD: Chapter 5

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -10,14 +10,14 @@ import {
 } from '../reducers/user';
 
 function logInAPI(data) {
-  return axios.post('/api/login', data);
+  return axios.post('/user/login', data);
 }
 function* logIn(action) {
   try {
     const result = yield call(logInAPI, action.data);
     yield put({
       type: LOG_IN_SUCCESS,
-      data: action.data,
+      data: result.data,
     });
   } catch (err) {
     yield put({
@@ -46,7 +46,7 @@ function* logOut() {
 }
 
 function signUpAPI(data) {
-  return axios.post('http://localhost:3065/user', data);
+  return axios.post('/user', data);
 }
 function* signUp(action) {
   try {
